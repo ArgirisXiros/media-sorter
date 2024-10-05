8a8c6a0e581379e788b2b461d46838495e00b22b
ArgirisXiros <xirosar@gmail.com>
2024-10-05 13:03:21 +0300

    Initial commit

---
+++ src/UI/MediaSorter.UI.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MediaSorter.UI.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
